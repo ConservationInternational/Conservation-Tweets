90ff2bad8704ce5eb0f6d72ee72b7f07a35736c3
mcooperci <mw.coop.r@gmail.com>
2016-09-22 16:22:09 -0400

    Script for collecting sample tweets in R

---
+++ GetSamples.R
@@ -0,0 +1,26 @@
+library(twitteR)
+library(dplyr)
+
+setwd('D:/Documents and Settings/mcooper/GitHub/Conservation-Tweets')
+
+source('llaves.R')
+
+setup_twitter_oauth(consumerKey, consumerSecret, accessToken, accessSecret)
+
+IndWordList <- c('kebakaran hutan', 'hakutla', 'sampah', 'terumbu karang', 'pari manta', 'harimau', 'gajah', 'perubahan iklim', 'gas rumah kaca', 'hiu paus')
+
+#Started at 1:07pm
+IndResultsAll <- data.frame()
+repeat{
+  for (i in IndWordList){
+    try({IndResults <- searchTwitter(searchString=i, n=1000, retryOnRateLimit=10, geocode='-2.755964,120.8247856,1000mi')
+    IndResultsDf <- twListToDF(IndResults)})
+    IndResultsAll <- unique(bind_rows(IndResultsAll, IndResultsDf))
+    print(i)
+    Sys.sleep(15)
+  }
+  print(paste0("We've got ", nrow(IndResultsAll), " Bahasa tweets"))
+  plot(IndResultsAll$longitude, IndResultsAll$latitude)
+  IndResultsAll %>% distinct()
+  write.csv(IndResultsAll, 'IndResultsAll.csv', row.names=F)
+}
