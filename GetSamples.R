05041b1d2e4afbb9c5ab95038c3d0573d842aa86
mcooperci <mw.coop.r@gmail.com>
2016-09-23 12:00:47 -0400

    fix spelling error

--- GetSamples.R
+++ GetSamples.R
@@ -7,7 +7,7 @@ source('llaves.R')
 
 setup_twitter_oauth(consumerKey, consumerSecret, accessToken, accessSecret)
 
-IndWordList <- c('kebakaran hutan', 'hakutla', 'sampah', 'terumbu karang', 'pari manta', 'harimau', 'gajah', 'perubahan iklim', 'gas rumah kaca', 'hiu paus')
+IndWordList <- c('kebakaran hutan', 'kahutla', 'sampah', 'terumbu karang', 'pari manta', 'harimau', 'gajah', 'perubahan iklim', 'gas rumah kaca', 'hiu paus')
 
 #Started at 1:07pm
 IndResultsAll <- data.frame()
@@ -15,12 +15,12 @@ repeat{
   for (i in IndWordList){
     try({IndResults <- searchTwitter(searchString=i, n=1000, retryOnRateLimit=10, geocode='-2.755964,120.8247856,1000mi')
     IndResultsDf <- twListToDF(IndResults)})
-    IndResultsAll <- unique(bind_rows(IndResultsAll, IndResultsDf))
+    IndResultsDf$key <- i
+    IndResultsAll <- unique(bind_rows(IndResultsAll, IndResultsDf[ , c('key', 'text', 'latitude', 'longitude')]))
     print(i)
     Sys.sleep(15)
   }
   print(paste0("We've got ", nrow(IndResultsAll), " Bahasa tweets"))
   plot(IndResultsAll$longitude, IndResultsAll$latitude)
-  IndResultsAll %>% distinct()
   write.csv(IndResultsAll, 'IndResultsAll.csv', row.names=F)
 }
