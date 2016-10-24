a85482fd5829a1b0b65773fb217c1be133af0084
mcooperci <mw.coop.r@gmail.com>
2016-10-24 10:45:37 -0400

    Script to read CSV files

--- ReadCsvs.R
+++ ReadCsvs.R
@@ -4,9 +4,6 @@ files <- list.files()
 
 t <- files[1]
 
-out <- read.csv(t)
-
-f <- readLines(t)
 
 #START including uuids in tweets
 #START removing \n from tweets
@@ -15,10 +12,27 @@ f <- readLines(t)
 #Hand in swahili?
 
 readCSV <- function(t){
-  f <- readLines(t)
-  header <- strsplit(f[1], ',')[[1]]
-  commaCounts <- sapply(f[2:length(f)], FUN=commaCount)
+  lines <- readLines(t)
+  commaCounts <- sapply(lines, FUN=commaCount)
+  clean <- paste(lines[commaCounts==14], collapse='\n')
+  df <- read.csv(textConnection(clean))
   
+  #deal with bad lines
+  sel <- lines[commaCounts!=14]
+  tweet <- ''
+  for (i in sel){
+    if(tweet==''){
+      tweet<-i
+    }else{
+      tweet <- paste0(tweet, i)
+    }
+    
+    if(length(gregexpr(',', tweet)[[1]])==14){
+      out <- parse14(tweet, names(df))
+      df <- rbind(df, out)
+      tweet <- ''
+    }
+  }
 }
 
 commaCount <- function(x){
@@ -32,6 +46,21 @@ parse14 <- function(l, header){
   df
 }
 
-
+textToBagOfWords <- function(str){
+  
+  clean <- gsub('[^[:alnum:] @#]', '', str)
+  
+  words <- strsplit(clean, ' ')[[1]]
+  
+  #turn urls to 'http'
+  inds <- unlist(gregexpr('http', words))
+  if (sum(inds[inds>0]) > 0){
+    words <- c(words[which(inds!=1)], 'http')
+  }
+  
+  words <- unique(words[words!=''])
+  
+  words
+}
 
 ## try a different method by gsubbing \n and parsing that
