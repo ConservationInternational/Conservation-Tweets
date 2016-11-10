9fea491ad59bb9fe28aa6d9a5d4a5f63b4c49cd7
mcooperci <mw.coop.r@gmail.com>
2016-11-10 14:57:12 -0500

    Tag tweets locally, then process and write

--- collect/writeS3.py
+++ collect/writeS3.py
@@ -4,50 +4,57 @@ import datetime
 import json
 
 files = os.listdir('temp_tweets')
-
-data = []
-for i in files:
-    try:
-        f = open('temp_tweets/' + i, 'r')
-        out = json.loads(f.read())
-        f.close()
-        twt = [out.get('text'),
-                out.get('id_str'),
-                out.get('coordinates'),
-                out.get('created_at'),
-                out.get('favorite_count'),
-                out.get('favorited'),
-                out.get('geo'),
-                out.get('place'),
-                out.get('retweet_count'),
-                out.get('retweeted'),
-                out.get('lang'),
-                out.get('user').get('favourites_count'),
-                out.get('user').get('friends_count'),
-                out.get('user').get('geo_enabled'),
-                out.get('user').get('location'),
-                out.get('user').get('name'),
-                out.get('user').get('statuses_count')]
-        twt = '@^@#%*^&%*$('.join(map(unicode, twt))
-        twt = twt.replace(',', ' ').replace('\n', ' ').replace('\r', ' ').replace('@^@#%*^&%*$(', ',')
-        data.append(twt)
-    except:
-        pass
-
-    #os.remove('temp_tweets/' + i)
     
 f = open('keywords.txt', 'r')
 keywords = f.read().splitlines()
 f.close()
 
-for k in keywords:
-    tempdat = ['text,id_str,coordinates,created_at,favorite_count,favorited,geo,place,retweet_count,retweeted,lang,user.favourites_count,user.friends_count,user.geo_enabled,user.location,user.name,user.statuses_count']
+for f in files:
+
+    t = open('temp_tweets/' + f, 'r')
+    raw = t.read()    
     
-    ks = k.split('&')
-       
-    for t in data:
-        if all(x.lower() in t[:t.find(',')].lower() for x in ks):
-            tempdat.append(t)
-    if len(tempdat) > 1:    
-        s3 = boto3.resource('s3')
-        s3.Bucket('ci-tweets').put_object(Key='ByKeyword/' + k.replace(' ', '.') + '-' + str(datetime.datetime.now())[:10] + '.csv',  Body='\n'.join(tempdat))
+    for k in keywords:
+        ks = k.split('&')
+        if all(raw.lower().find(x.lower()) > -1 for x in ks):
+            f.write('tagged_tweets/' + k + f)
+
+tagged_files = os.listdir('tagged_tweets')
+
+for k in keywords:
+
+    data = ['text,id_str,coordinates,created_at,favorite_count,favorited,geo,place,retweet_count,retweeted,lang,user.favourites_count,user.friends_count,user.geo_enabled,user.location,user.name,user.statuses_count']    
+    
+    for fl in tagged_files:
+        if k in fl:    
+            try:
+                f = open('tagged_tweets/' + f, 'r')
+                out = json.loads(f.read())
+                f.close()
+                twt = [out.get('text'),
+                        out.get('id_str'),
+                        out.get('coordinates'),
+                        out.get('created_at'),
+                        out.get('favorite_count'),
+                        out.get('favorited'),
+                        out.get('geo'),
+                        out.get('place'),
+                        out.get('retweet_count'),
+                        out.get('retweeted'),
+                        out.get('lang'),
+                        out.get('user').get('favourites_count'),
+                        out.get('user').get('friends_count'),
+                        out.get('user').get('geo_enabled'),
+                        out.get('user').get('location'),
+                        out.get('user').get('name'),
+                        out.get('user').get('statuses_count')]
+                twt = '@^@#%*^&%*$('.join(map(unicode, twt))
+                twt = twt.replace(',', ' ').replace('\n', ' ').replace('\r', ' ').replace('@^@#%*^&%*$(', ',')
+                if all(x.lower() in twt[:twt.find(',')].lower() for x in ks):
+                    data.append(twt)
+            except:
+                pass    
+        
+        if len(data) > 1:    
+            s3 = boto3.resource('s3')
+            s3.Bucket('ci-tweets').put_object(Key='test/' + k.replace(' ', '.') + '-' + str(datetime.datetime.now())[:10] + '.csv',  Body='\n'.join(data))
