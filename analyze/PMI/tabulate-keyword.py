427f639c4b60af4daffeb342852e1107337ce4e8
mcooperci <mw.coop.r@gmail.com>
2017-01-11 12:02:17 -0500

    refactor

--- analyze/PMI/tabulate-keyword.py
+++ analyze/PMI/tabulate-keyword.py
@@ -6,10 +6,10 @@ Created on Mon Dec 19 16:19:22 2016
 """
 
 #Paramaters to set
-searchkey = 'climate.change'
-searchstart = '2016-11-09'
-searchend = '2016-12-31'
-saveas = 'climatechange-post'
+searchkey = 'vegetarian'
+searchstart = '2016-09-06'
+searchend = 99
+saveas = 'vegetarian'
 
 import boto3
 import pickle
@@ -44,49 +44,55 @@ def generatefiles(keyword, start='2016-09-06', end=99):
     
     return result
 
-files = generatefiles(searchkey, searchstart, searchend)
 
-#Locally connect to s3 and read stopwords
-if sys.platform == 'win32':
-    f = open('D:/Documents and Settings/mcooper/.aws/credentials')
-    read = f.readlines()
-    access_key = read[1][20:-1]
-    secret_key = read[2][24:-1]
-    s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-    
-    stopwords = []    
-    f = open('D:/Documents and Settings/mcooper/GitHub/Conservation-Tweets/analyze/stopwords.txt')
-    for i in f.readlines():
-        stopwords.append(i.rstrip('\n'))
+def tabulate(files):
+
+    if sys.platform == 'win32':
+        f = open('D:/Documents and Settings/mcooper/.aws/credentials')
+        read = f.readlines()
+        access_key = read[1][20:-1]
+        secret_key = read[2][24:-1]
+        s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
         
-elif sys.platform == 'linux2':
-    s3 = boto3.client('s3')
+        stopwords = []    
+        f = open('D:/Documents and Settings/mcooper/GitHub/Conservation-Tweets/analyze/stopwords.txt')
+        for i in f.readlines():
+            stopwords.append(i.rstrip('\n'))
+            
+    elif sys.platform == 'linux2':
+        s3 = boto3.client('s3')
+        
+        stopwords = []    
+        f = open('~/stopwords.txt')
+        for i in f.readlines():
+            stopwords.append(i.rstrip('\n'))
     
-    stopwords = []    
-    f = open('~/stopwords.txt')
-    for i in f.readlines():
-        stopwords.append(i.rstrip('\n'))
+    conditionaldict = defaultdict(int)
+    for f in files:
+        print('now on file ' + f)
+        try:
+            out = s3.get_object(Bucket='ci-tweets', Key=f)
+            file = pandas.read_csv(out['Body'])
+            lines = file["text"].tolist()
+            for l in lines:
+                l = re.sub(r'[^a-z#@ ]', '', l.lower())
+                words = l.split()
+                for w in set(words):
+                    if w=='rt' or w in stopwords:
+                        pass
+                    elif w[:4]=='http':
+                        conditionaldict['http'] += 1
+                    else:
+                        conditionaldict[w] += 1
+                conditionaldict['TOTAL'] += 1
+        except:
+            print('File ' + f + ' was skipped')
 
-conditionaldict = defaultdict(int)
-for f in files:
-    print('now on file ' + f)
-    try:
-        out = s3.get_object(Bucket='ci-tweets', Key=f)
-        file = pandas.read_csv(out['Body'])
-        lines = file["text"].tolist()
-        for l in lines:
-            l = re.sub(r'[^a-z#@ ]', '', l.lower())
-            words = l.split()
-            for w in set(words):
-                if w=='rt' or w in stopwords:
-                    pass
-                elif w[:4]=='http':
-                    conditionaldict['http'] += 1
-                else:
-                    conditionaldict[w] += 1
-            conditionaldict['TOTAL'] += 1
-    except:
-        print('File ' + f + ' was skipped')
+    return conditionaldict
 
 
-pickle.dump(conditionaldict, open(saveas + ".p", "wb"), protocol=2)
+if __name__=='__main__':
+    files = generatefiles(searchkey, searchstart, searchend)
+    tabulation = tabulate(files)
+    pickle.dump(tabulation, open(saveas + ".p", "wb"), protocol=2)
+
