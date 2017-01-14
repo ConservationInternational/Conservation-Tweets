7ace7e95045917f2546ab6237ccf5d682feb5a36
Matt Cooper <mcooper@conservation.org>
2017-01-14 14:28:54 -0500

    generate files with PMI and wordcount

---
+++ analyze/PMI/generate-PMI.py
@@ -0,0 +1,51 @@
+import pandas
+import math
+import boto3
+import pickle
+import csv
+
+#Read in data, skipping words already in buck
+s3client = boto3.client('s3')
+s3resource = boto3.resource('s3')
+
+paginator = s3client.get_paginator('list_objects')
+pages = paginator.paginate(Bucket='ci-tweets-wordcounts', Prefix='')
+files = []
+for p in pages:
+    for x in p['Contents']:
+        files.append(x['Key'])
+
+skipfiles = []
+try:
+    pages = paginator.paginate(Bucket='ci-tweet-PMI', Prefix='')
+    for p in pages:
+        for x in p['Contents']:
+            skipfiles.append(x['Key'])
+except:
+    for object in s3resource.Bucket('ci-tweet-PMI').objects.all():
+        print(object)
+
+for f in skipfiles:
+    files.remove(f)
+
+baseline = dict(pickle.load(open("baseline_2017-01-06_prob.p", "rb")))
+
+def getPMI(df, total):
+    try:
+        base = baseline[df['word']]
+        PMI = math.log((df['count']/total)/base)
+        return(PMI)
+    except:
+        return(0)
+            
+for f in files:
+    out = s3client.get_object(Bucket='ci-tweet-wordcounts', Key=f)
+    df = pandas.read_csv(out['Body'], quoting=csv.QUOTE_NONE, error_bad_lines=False, warn_bad_lines=True, names=['word', 'count'])
+                
+    total = int(df.loc[df['word']=='TOTAL','count'])
+    
+    df['PMI'] = df.apply(getPMI, axis=1, total=total)
+
+    s3resource.Bucket('ci-tweet-PMI').put_object(Key=f, Body=df.to_csv(None, encoding='utf-8', index=False))
+
+
