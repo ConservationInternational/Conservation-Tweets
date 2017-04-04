a780984112a83809a7c10cc860ab2b0c7240c3a4
mcooperci <mw.coop.r@gmail.com>
2017-04-04 11:24:33 -0400

    Unique variable name for writing errors

--- analyze/PMI/generate-word-counts.py
+++ analyze/PMI/generate-word-counts.py
@@ -1,16 +1,10 @@
-#Separate out geographic tweets
-#Keep only columns id_str, geo, country, state, county, city, latitude, longitude
-#Split coords into exact.latitude & exact.longitude
-#maybe also a any.latitude and any.longitude file
-#group by keyword and write
-
-#Run on 1-13-2017
-
 import boto3
 import pandas
 import csv
 from collections import defaultdict
 import re
+
+skip_already_written = False
 
 #Read in data, skipping words already in buck
 s3client = boto3.client('s3')
@@ -23,32 +17,41 @@ for p in pages:
     for x in p['Contents']:
         files.append(x['Key'])
 
-skipfiles = []
-try:
-    pages = paginator.paginate(Bucket='ci-tweet-wordcounts', Prefix='')
-    for p in pages:
-        for x in p['Contents']:
-            skipfiles.append(x['Key'])
-except:
-    for object in s3resource.Bucket('ci-tweet-wordcounts').objects.all():
-        print(object)
 
-skipkeys = []
-for f in skipfiles:
-    skipkeys.append(f[:-4])
+if skip_already_written:
+	skipfiles = []
+	try:
+		pages = paginator.paginate(Bucket='ci-tweet-wordcounts', Prefix='')
+		for p in pages:
+			for x in p['Contents']:
+				skipfiles.append(x['Key'])
+	except:
+		for object in s3resource.Bucket('ci-tweet-wordcounts').objects.all():
+			print(object)
 
-keys = []
-for f in files[1:]:
-    new = f[10:-15]
-    if (new not in keys) and (new not in skipkeys):
-        keys.append(new)
-        
+	skipkeys = []
+	for f in skipfiles:
+		skipkeys.append(f[:-4])
+
+	keys = []
+	for f in files[1:]:
+		new = f[10:-15]
+		if (new not in keys) and (new not in skipkeys):
+			keys.append(new)
+			
+else:
+	keys = []
+	for f in files[1:]:
+		new = f[10:-15]
+		if (new not in keys) :
+			keys.append(new)	
+
 #get stopwords
 stopwords = []
 f = open('stopwords.txt')
 for i in f.readlines():
     stopwords.append(i.rstrip('\n'))
-            
+
 for k in keys:
     wc = defaultdict(int)
     for f in files[1:]:
@@ -71,17 +74,15 @@ for k in keys:
                             wc[w] += 1
                     wc['TOTAL'] += 1
             except:
-                f = open('wc-fail.txt')
-                f.write(f + '\n')
-                f.close()   
-                
+                w = open('wc-fail.txt')
+                w.write(f + '\n')
+                w.close()   
     string = ''
     for i in wc:
         string += i
         string += ','
         string += str(wc[i])
         string += '\n'
-
     s3resource.Bucket('ci-tweet-wordcounts').put_object(Key=(k + '.csv'),  Body=string)
 
 
