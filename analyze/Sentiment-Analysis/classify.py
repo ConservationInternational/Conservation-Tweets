2da1870b98ae4b3664dca9fd5847e8b3a939fd0b
Matt Cooper <mcooper@conservation.org>
2017-04-12 19:40:26 -0400

    Validate and being to classify

---
+++ analyze/Sentiment-Analysis/classify.py
@@ -0,0 +1,50 @@
+from __future__ import division
+import pandas as pd
+import boto3
+import csv
+
+s3client = boto3.client('s3')
+s3resource = boto3.resource('s3')
+
+paginator = s3client.get_paginator('list_objects')
+pages = paginator.paginate(Bucket='ci-tweets', Prefix='ByKeyword')
+files = []
+for p in pages:
+    for x in p['Contents']:
+        files.append(x['Key'])
+
+dates = []
+sel_files = []
+for f in files:
+    if f[10:-15] in ['global.warming', 'globalwarming', 'climate.change', 'climatechange']:
+        sel_files.append(f)
+    if f[-14:-4] not in dates:
+        dates.append(f[-14:-4])
+
+exec(open('classifier.py', 'rb').read())
+c = Classifier(pd.read_csv('Climate_trainingdata_classified.csv'), tokens=1)
+
+for d in dates[1:]:
+    
+    print(d)
+    
+    date_files = []
+    for f in sel_files:
+        if d in f:
+            date_files.append(f)
+            
+    df = pd.DataFrame()
+    for f in date_files:
+        out = s3client.get_object(Bucket='ci-tweets', Key=f)
+        temp = pd.read_csv(out['Body'], quoting=csv.QUOTE_NONE, error_bad_lines=False, warn_bad_lines=True)
+        
+        temp['keyword'] = f[10:-15]
+        
+        df = df.append(temp, ignore_index = True)
+    
+    print('Classifying')
+    
+    df[c.classes] = df['text'].apply(c.classify)
+    df['max'] = df[c.classes].idxmax(axis=1)
+    
+    s3resource.Bucket('ci-tweets-sentiment').put_object(Key='climate/climate' + d + '.csv', Body=df.to_csv(None, encoding='utf-8', index=False))
--- analyze/Sentiment-Analysis/validation/validate.py
+++ analyze/Sentiment-Analysis/validation/validate.py
@@ -8,28 +8,15 @@ Created on Thu Apr  6 10:31:49 2017
 import os
 import pandas as pd
 import numpy as np
-import classifier
 
 os.chdir('D:/Documents and Settings/mcooper/GitHub/Conservation-Tweets/analyze/Sentiment-Analysis')
 
-df = pd.read_csv('Climate_trainingdata.csv')
+exec(open('classifier.py', 'rb').read())
 
-df['sel'] = np.random.choice([1,2], df.shape[0], p=[0.8, 0.2])
-
-
-training = df.loc[df['sel']==1]
-
-test = df.loc[df['sel']==2]
-
-c = Classifier(training, tokens=1)
-
-test[c.classes] = test['text'].apply(c.classify)
-
-test['max'] = test[c.classes].idxmax(axis=1)
-
-test = test.dropna()
-
-ct = pd.crosstab(test['class'], test['max'])
+def accuracy(ct):
+    N = ct.sum().sum()
+    p0 = np.diag(ct).sum()/N
+    return(p0)
 
 def kappa(ct):
     N = ct.sum().sum()
@@ -37,5 +24,35 @@ def kappa(ct):
     pE = (1/N**2)*(ct.sum(axis=1)*ct.sum(axis=0)).sum()
     k = (p0 - pE)/(1 - pE)
     return(k)
+
+
+def testOne(df, p=[0.8, 0.2], tok=1):
+    df['sel'] = np.random.choice([1,2], df.shape[0], p=[0.8, 0.2])
+
+    training = df.loc[df['sel']==1]
+    test = df.loc[df['sel']==2]
     
-kappa(ct)
+    c = Classifier(training, tokens=tok)
+    
+    test[c.classes] = test['text'].apply(c.classify)
+    test['max'] = test[c.classes].idxmax(axis=1)
+    test = test.dropna()
+    
+    ct = pd.crosstab(test['class'], test['max'])
+    
+    return(ct)
+
+def testAccuracy(df, p=[0.8, 0.2], tok=1, n=20):
+    cts = []    
+    for i in range(1,n): 
+        print(i)
+        cts.append(testOne(df, tok=tok, p=p))        
+    
+    return(sum(cts)/len(cts))
+
+#only data trained by Matt, 1 token
+kappa(testAccuracy(pd.read_csv('Climate_trainingdata.csv'), tok=2, n=20))
+#0.435
+
+accuracy(testAccuracy(pd.read_csv('Climate_trainingdata.csv'), tok=2, n=20))
+#0.770
